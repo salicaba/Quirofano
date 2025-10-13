6ab61034cb1b12fad9d1b55e4cca84df0e767159
GAF3R0 <gaf3r0.pr0g@gmail.com>
2025-10-13 10:31:39 -0600

    CRUD-PACIENTES

--- Frontend/.src/components/frontabmodel/src/main.jsx
+++ Frontend/.src/components/frontabmodel/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'; // <-- ESTA LÍNEA ES LA CLAVE QUE FALTABA
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App.jsx';
+//hola
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
